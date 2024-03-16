e5b995700fed539d50361fbb25358dab948d5d95
mitsu <mitsuyoshi-sato4256@gmail.com>
2024-03-16 23:39:01 +0900

    いらないファイル削除

--- app/likes/data.ts
+++
@@ -1,8 +0,0 @@
-
-
-export const likes = [
-  {name:'サンプルサイト', comment:'~tamago.puroject~', link:'https://tamago-project.vercel.app/'},
-  {name:'Git Hub', comment:'〜 見学お待ちしております 〜',link:'https://github.com/mitsuyoshi-sato'},
-  {name:'Crowd Works', comment:'〜 仕事募集中 〜',link:'https://crowdworks.jp/dashboard'},
-  {name:'サンプル作成中', comment:'〜 サンプル サンプル 〜'},
-]
