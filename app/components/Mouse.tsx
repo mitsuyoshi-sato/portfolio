d0104689bd29ff4a36982803700c2c29639e4186
mitsu <mitsuyoshi-sato4256@gmail.com>
2024-03-17 13:22:38 +0900

    全体の体裁整えた

--- app/components/Mouse.tsx
+++ app/components/Mouse.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import Link from 'next/link'
-import { likes } from '../data/data'
 import { useEffect, useRef } from 'react';
 import clsx from 'clsx';
 
@@ -14,31 +13,48 @@ export default function Mouse() {
     const i = imgRef.current
     if (d && i) {
       d.onmousemove = (e) => {
+        // const r = d.getBoundingClientRect()
         const x = e.clientX
         const y = e.clientY
+        i.style.display = 'block'
+        i.style.transform = `translate(${x}px, ${y}px)`
+      }
+      d.onmouseleave = () =>{
+        i.style.display = 'none'
       }
     }
-  })
+  }, [])
 
   return(
     <ul className="mt-6 grid grid-cols-2 gap-4">
-    {likes.map((like:any) => {
-      return (
-        <li key={like.name}>
-          <Link href={`${like.link}`}>
-            <div ref={divRef} className="cont h-[200px] w-[500px] rounded-lg p-8">
-              <p className="mt-3 opacity-70">{like.date}</p>
-              <p className="mt-1 text-[18px]">{like.name}</p>
-              <p className="mt-3 opacity-70">{like.comment}</p>
-              <img
-                ref={imgRef}
-                src="tamago.jpg"
-              />
-            </div>
-          </Link>
-        </li>
-      )
-    })}
-  </ul>
+      <li>
+        <Link href="https://tamago-project.vercel.app/">
+          <div ref={divRef} className="cont h-[200px] w-[500px] rounded-lg p-8">
+            <p className="mt-3 opacity-70">April 2022</p>
+            <p className="mt-1 text-[18px]">金の卵株式会社</p>
+            <p className="mt-3 opacity-70">2022年4月、金の卵株式会社のホームページを作成しました。</p>
+            <img
+              ref={imgRef}
+              src="tamago.jpg"
+              className="absolute left-[-200px] top-[-125px] hidden h-[250px] w-[400px] rounded-lg"
+            />
+          </div>
+        </Link>
+      </li>
+      <li>
+        <Link href="">
+          <div ref={divRef} className="cont h-[200px] w-[500px] rounded-lg p-8">
+            <p className="mt-3 opacity-70">Coming Soon...</p>
+            <p className="mt-1 text-[18px]">Port Forio Tool</p>
+            <p className="mt-3 opacity-70">誰でも簡単にポートフォリオが作成できる便利なツールを作成しております。近日公開予定。</p>
+            <img
+              ref={imgRef}
+              src="coming.jpg"
+              className="absolute left-[-200px] top-[-125px] hidden h-[250px] w-[400px] rounded-lg object-cover"
+            />
+          </div>
+        </Link>
+      </li>
+    </ul>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -14,11 +14,13 @@ export default function Page() {
         <div className="absolute left-[200px] top-[200px] text-[6.8vw] leading-[7vw] text-white/90 opacity-70">Design &<br/> Front-End<br/> Developer</div>
       </div>
       <div className="mx-auto w-[calc(100%-400px)]">
-        <p className="mt-8 text-center text-[5vw] font-semibold">Works</p>
-        <Mouse />
-        <p className="mt-36 border-b-2 border-b-black/70 pb-4 text-6xl" id="about">About</p>
-        <div className="mt-12 flex leading-8">
-          <p className="ml-10">
+      <p className="mb-14 mt-36 border-b-2 border-b-[#aa8875] pb-4 text-6xl" id="about">Works</p>
+        <div className="mx-auto w-[calc(100%-100px)]">
+          <Mouse />
+        </div>
+        <p className="mt-36 border-b-2 border-b-[#aa8875] pb-4 text-6xl" id="about">About</p>
+        <div className="mx-auto mt-12 flex w-[calc(100%-100px)] leading-8">
+          <p>
             【経歴】<br/>
             ・2021年から個人事業主として活動開始。<br/>
             ・2022年からニッタスタジオ株式会社に勤務。<br/>
@@ -42,15 +44,21 @@ export default function Page() {
             </p>
           </div>
         </div>
-        <p className="mt-36 border-b-2 border-b-black pb-4 text-6xl" id="contact">Contact</p>
-        <p className="ml-10 mt-10">ご依頼等、お気軽にお待ちしております！</p>
-        <button className="ml-10 mt-4 flex gap-1 rounded-md border-2 border-black/70 px-1 py-2">
-          <p>Send Mail</p>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="h-6 w-6">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
-          </svg>
-        </button>
       </div>
+        <div className=" mt-10 h-[300px] w-full bg-[#282420] pt-14">
+          <div className="mx-auto w-[calc(100%-400px)]">
+            <p className="border-b-2 border-b-[#aa8875] pb-4 text-6xl" id="contact">Contact</p>
+            <div className="mx-auto flex h-32 w-[calc(100%-100px)] items-center gap-3">
+              <button className="flex gap-1 rounded-md border-2 border-[#aa8875] px-1 py-2">
+                <p>Send Mail</p>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="h-6 w-6">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
+                </svg>
+              </button>
+              <p>ご依頼等、お気軽にお待ちしております！</p>
+            </div>
+          </div>
+        </div>
       <Header />
     </>
   )
