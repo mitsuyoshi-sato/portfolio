ac043ea76b0e3ebcbf4dc315044c199cf3b65df2
mitsu <mitsuyoshi-sato4256@gmail.com>
2024-03-17 23:02:16 +0900

    モバイル版、worksまで修正完了。

--- app/components/Mouse.tsx
+++ app/components/Mouse.tsx
@@ -26,10 +26,10 @@ export default function Mouse() {
   }, [])
 
   return(
-    <ul className="mt-6 grid grid-cols-2 gap-4">
+    <ul className="mt-6 grid-cols-2 gap-4 lg:grid">
       <li>
         <Link href="https://tamago-project.vercel.app/">
-          <div ref={divRef} className="cont h-[200px] w-[500px] rounded-lg p-8">
+          <div ref={divRef} className="cont mx-auto w-[80%] max-w-[600px] rounded-lg p-8 lg:h-[200px] lg:w-auto">
             <p className="mt-3 opacity-70">April 2022</p>
             <p className="mt-1 text-[18px]">金の卵株式会社</p>
             <p className="mt-3 opacity-70">2022年4月、金の卵株式会社のホームページを作成しました。</p>
@@ -41,9 +41,9 @@ export default function Mouse() {
           </div>
         </Link>
       </li>
-      <li>
+      <li className="mt-10 lg:mt-0">
         <Link href="">
-          <div ref={divRef} className="cont h-[200px] w-[500px] rounded-lg p-8">
+          <div ref={divRef} className="cont mx-auto w-[80%] max-w-[600px] rounded-lg p-8 lg:h-[200px] lg:w-auto">
             <p className="mt-3 opacity-70">Coming Soon...</p>
             <p className="mt-1 text-[18px]">Port Forio Tool</p>
             <p className="mt-3 opacity-70">誰でも簡単にポートフォリオが作成できる便利なツールを作成しております。近日公開予定。</p>
--- app/page.tsx
+++ app/page.tsx
@@ -9,14 +9,13 @@ export default function Page() {
       <div className="relative overflow-hidden">
         <img src="pc.jpg" className="h-[600px] w-full object-cover"/>
         <div className="absolute inset-0 bg-black opacity-25"/>
-        <div className="absolute left-[200px] top-[200px] text-[6.8vw] leading-[7vw] text-white/90 opacity-70">Design &<br/> Front-End<br/> Developer</div>
+        <div className="absolute left-[20%] top-[200px] text-[70px] leading-[72px] text-white/90 opacity-70 lg:text-[6.8vw] lg:leading-[7vw]">Design &<br/> Front-End<br/> Developer</div>
       </div>
-      <div className="mx-auto w-[calc(100%-400px)]">
-      <p className="mb-14 mt-36 border-b-2 border-b-[#aa8875] pb-4 text-6xl" id="about">Works</p>
-        <div className="mx-auto w-[calc(100%-100px)]">
+      <p className="mx-auto mb-14 mt-36 w-[165px] min-w-[165px] border-b-2 border-b-[#aa8875] pb-4 text-6xl lg:w-[80%]" id="about">Works</p>
+        <div className="mx-auto w-[80%]">
           <Mouse />
         </div>
-        <p className="mt-36 border-b-2 border-b-[#aa8875] pb-4 text-6xl" id="about">About</p>
+        <p className="mx-auto mb-14 mt-36 w-[160px] min-w-[160px] border-b-2 border-b-[#aa8875] pb-4 text-6xl lg:w-[80%]" id="about">About</p>
         <div className="mx-auto mt-12 flex w-[calc(100%-100px)] leading-8">
           <p>
             【経歴】<br/>
@@ -42,19 +41,16 @@ export default function Page() {
             </p>
           </div>
         </div>
-      </div>
-        <div className=" mt-10 h-[300px] w-full bg-[#282420] pt-14">
-          <div className="mx-auto w-[calc(100%-400px)]">
-            <p className="border-b-2 border-b-[#aa8875] pb-4 text-6xl" id="contact">Contact</p>
-            <div className="mx-auto flex h-32 w-[calc(100%-100px)] items-center gap-3">
-              <button className="flex gap-1 rounded-md border-2 border-[#aa8875] px-1 py-2">
-                <p>Send Mail</p>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="h-6 w-6">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
-                </svg>
-              </button>
-              <p>ご依頼等、お気軽にお待ちしております！</p>
-            </div>
+        <div className="mt-10 h-auto w-full bg-[#282420] pt-14">
+          <p className="mx-auto mb-14 mt-36 w-[200px] min-w-[200px] border-b-2 border-b-[#aa8875] pb-4 text-6xl lg:w-[80%]" id="contact">Contact</p>
+          <div className="mx-auto flex h-32 w-[calc(100%-100px)] items-center gap-3">
+            <button className="flex gap-1 rounded-md border-2 border-[#aa8875] px-1 py-2">
+              <p>Send Mail</p>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="h-6 w-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
+              </svg>
+            </button>
+            <p>ご依頼等、お気軽にお待ちしております！</p>
           </div>
         </div>
       <Header />
